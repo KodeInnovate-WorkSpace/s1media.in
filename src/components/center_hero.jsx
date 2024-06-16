4e290073a8389f5b0910c88d08df96c1bf877630
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-06-16 14:19:49 +0530

    Final

--- src/components/center_hero.jsx
+++ src/components/center_hero.jsx
@@ -24,24 +24,11 @@ const CenterHero = ({ title, content, img }) => {
         <h1 className="display-4 fw-bold">{title}</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">{content}</p>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-            {/* <button type="button" className="btn btn-danger btn-lg px-4 me-sm-3">Primary button</button>
-                        <button type="button" className="btn btn-outline-danger btn-lg px-4">Secondary</button> */}
-          </div>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5"></div>
         </div>
       </div>
     </>
   );
 };
-
-/*
-*Welcome to S1Media*
-
-At S1Media, we specialize in showcasing your properties, restaurants, cooking tips, hotels, and agricultural ventures through high-quality videos. Our team of professional shooting editors is dedicated to capturing the essence of your business and sharing it with a wide audience on YouTube and various social media platforms. We offer two years of free marketing, and after that, our rates are incredibly affordable at just 50 RS per day.
-
-*Our Mission*
-Our mission is to provide businesses with a platform to reach a larger audience through engaging video content, enhancing their visibility and growth without upfront costs.
-
-*/
 
 export default CenterHero;
--- src/components/service_card.jsx
+++ src/components/service_card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Card, Col, ListGroup } from "react-bootstrap";
+import { Slide } from "react-awesome-reveal";
+import { Card, Col, Row, ListGroup } from "react-bootstrap";
 
 export const ServiceCard = () => {
   return (
     <>
-      <section className="bg-black  py-3 py-md-5 py-xl-8">
+      <section className="bg-black py-3 py-md-5 py-xl-8">
         <div className="container">
           <div className="row justify-content-md-center">
             <div className="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
@@ -23,145 +24,161 @@ export const ServiceCard = () => {
           <div className="row">
             <div className="col-12">
               <div className="container-fluid">
-                <div className="row gy-3 gy-md-4">
-                  {/* Card 1  */}
-                  <Col lg={4} className="col-12">
-                    <Card className="card border-dark">
-                      <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
-                        <div className="me-3 text-primary">
-                          <img
-                            src="real_estate.svg"
-                            width={48}
-                            height={48}
-                            alt="Real Estate"
-                          />
-                        </div>
-                        <div>
-                          <h4 className="mb-1 text-white">Real Estate</h4>
+                <Row className="gy-3 gy-md-4 d-md-ms-2">
+                  <Slide>
+                    <Row>
+                      {/* Card 1  */}
+                      <Col lg={4} md={6} className="col-12 mb-3">
+                        <Card className="card border-dark">
+                          <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
+                            <div className="me-3 text-primary">
+                              <img
+                                src="real_estate.svg"
+                                width={48}
+                                height={48}
+                                alt="Real Estate"
+                              />
+                            </div>
+                            <div>
+                              <h4 className="mb-1 text-white">Real Estate</h4>
+                              <ul
+                                className="text-secondary"
+                                style={{ listStyle: "none", padding: "0" }}
+                              >
+                                <li>Video tours of properties</li>
+                                <li>
+                                  Highlighting unique features and amenities
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Card>
+                      </Col>
 
-                          <ul
-                            className="text-secondary"
-                            style={{ listStyle: "none", padding: "0" }}
-                          >
-                            <li>Video tours of properties</li>
-                            <li>Highlighting unique features and amenities</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </Card>
-                  </Col>
-                  {/* Card 2  */}
+                      {/* Card 2  */}
+                      <Col lg={4} md={6} className="col-12">
+                        <Card className="card border-dark">
+                          <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
+                            <div className="me-3 text-primary">
+                              <img
+                                src="cooking.svg"
+                                width={48}
+                                height={48}
+                                alt="Cooking Tips"
+                              />
+                            </div>
+                            <div>
+                              <h4 className="mb-1 text-white">Cooking Tips</h4>
+                              <ul
+                                className="text-secondary"
+                                style={{ listStyle: "none", padding: "0" }}
+                              >
+                                <li>Step-by-step cooking videos</li>
+                                <li>
+                                  Featuring professional chefs and home cooks
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Card>
+                      </Col>
 
-                  <Col lg={4} className="col-12">
-                    <Card className="card border-dark">
-                      <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
-                        <div className="me-3 text-primary">
-                          <img
-                            src="restaurant.svg"
-                            width={48}
-                            height={48}
-                            alt="Real Estate"
-                          />
-                        </div>
-                        <div>
-                          <h4 className="mb-1 text-white">Restaurant</h4>
+                      {/* Card 3  */}
 
-                          <ul
-                            className="text-secondary"
-                            style={{ listStyle: "none", padding: "0" }}
-                          >
-                            <li>
-                              Capturing the ambiance and culinary delights
-                            </li>
-                            <li>Showcasing special dishes and events</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </Card>
-                  </Col>
-                  {/* Card 3  */}
-                  <Col lg={4} className="col-12">
-                    <Card className="card border-dark">
-                      <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
-                        <div className="me-3 text-primary">
-                          <img
-                            src="cooking.svg"
-                            width={48}
-                            height={48}
-                            alt="Real Estate"
-                          />
-                        </div>
-                        <div>
-                          <h4 className="mb-1 text-white">Cooking Tips</h4>
+                      <Col lg={4} md={6} className="col-12 mb-3">
+                        <Card className="card border-dark">
+                          <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
+                            <div className="me-3 text-primary">
+                              <img
+                                src="restaurant.svg"
+                                width={48}
+                                height={48}
+                                alt="Restaurant"
+                              />
+                            </div>
+                            <div>
+                              <h4 className="mb-1 text-white">Restaurant</h4>
+                              <ul
+                                className="text-secondary"
+                                style={{ listStyle: "none", padding: "0" }}
+                              >
+                                <li>
+                                  Capturing the ambiance and culinary delights
+                                </li>
+                                <li>Showcasing special dishes and events</li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Card>
+                      </Col>
+                    </Row>
+                  </Slide>
 
-                          <ul
-                            className="text-secondary"
-                            style={{ listStyle: "none", padding: "0" }}
-                          >
-                            <li>Step-by-step cooking videos</li>
-                            <li>Featuring professional chefs and home cooks</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </Card>
-                  </Col>
-                  {/* Card 4  */}
-                  <Col lg={4} className="col-12">
-                    <Card className="card border-dark">
-                      <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
-                        <div className="me-3 text-primary">
-                          <img
-                            src="hotel.svg"
-                            width={48}
-                            height={48}
-                            alt="Hotel"
-                          />
-                        </div>
-                        <div>
-                          <h4 className="mb-1 text-white">
-                            Hotels (3-star, 4-star, 5-star)
-                          </h4>
+                  <Slide>
+                    <Row>
+                      {/* Card 4  */}
+                      <Col lg={4} md={6} className="col-12">
+                        <Card className="card border-dark">
+                          <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
+                            <div className="me-3 text-primary">
+                              <img
+                                src="hotel.svg"
+                                width={48}
+                                height={48}
+                                alt="Hotel"
+                              />
+                            </div>
+                            <div>
+                              <h4 className="mb-1 text-white">
+                                Hotels (3-star, 4-star, 5-star)
+                              </h4>
+                              <ul
+                                className="text-secondary"
+                                style={{ listStyle: "none", padding: "0" }}
+                              >
+                                <li>Virtual tours of hotel facilities</li>
+                                <li>
+                                  Highlighting services and guest experience
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Card>
+                      </Col>
+                      {/* Card 5  */}
 
-                          <ul
-                            className="text-secondary"
-                            style={{ listStyle: "none", padding: "0" }}
-                          >
-                            <li>Virtual tours of hotel facilities</li>
-                            <li>Highlighting services and guest experience</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </Card>
-                  </Col>
-                  {/* Card 5  */}
-                  <Col lg={4} className="col-12">
-                    <Card className="card border-dark">
-                      <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
-                        <div className="me-3 text-primary">
-                          <img
-                            src="agriculture.svg"
-                            width={48}
-                            height={48}
-                            alt="Real Estate"
-                          />
-                        </div>
-                        <div>
-                          <h4 className="mb-1 text-white">Agriculture</h4>
-
-                          <ul
-                            className="text-secondary"
-                            style={{ listStyle: "none", padding: "0" }}
-                          >
-                            <li>
-                              Documenting agricultural processes and products
-                            </li>
-                            <li>Showcasing innovative farming techniques</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </Card>
-                  </Col>
-                </div>
+                      <Col lg={4} md={6} className="col-12">
+                        <Card className="card border-dark">
+                          <div className="card-body p-3 p-md-4 p-xxl-5 d-flex justify-content-center align-items-center">
+                            <div className="me-3 text-primary">
+                              <img
+                                src="agriculture.svg"
+                                width={48}
+                                height={48}
+                                alt="Agriculture"
+                              />
+                            </div>
+                            <div>
+                              <h4 className="mb-1 text-white">Agriculture</h4>
+                              <ul
+                                className="text-secondary"
+                                style={{ listStyle: "none", padding: "0" }}
+                              >
+                                <li>
+                                  Documenting agricultural processes and
+                                  products
+                                </li>
+                                <li>
+                                  Showcasing innovative farming techniques
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Card>
+                      </Col>
+                    </Row>
+                  </Slide>
+                </Row>
               </div>
             </div>
           </div>
@@ -174,24 +191,6 @@ export const ServiceCard = () => {
               <h2 className="mb-4 display-5 text-white text-center">
                 How it works
               </h2>
-
-              {/* <ListGroup className="bg-black">
-                <ListGroup.Item className="bg-black text-white">
-                  Our team visits your location to shoot high-quality video
-                  content.
-                </ListGroup.Item>
-                <ListGroup.Item className="bg-black text-white">
-                  We edit and produce engaging videos.
-                </ListGroup.Item>
-                <ListGroup.Item className="bg-black text-white">
-                  We upload the videos to our YouTube channel and share them on
-                  social media.
-                </ListGroup.Item>
-                <ListGroup.Item className="bg-black text-white">
-                  Enjoy two years of free marketing, with an option to continue
-                  for a small daily fee thereafter.
-                </ListGroup.Item>
-              </ListGroup> */}
 
               <ListGroup className="bg-black">
                 <ListGroup.Item
