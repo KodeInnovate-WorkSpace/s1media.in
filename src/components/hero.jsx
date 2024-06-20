12c4589da558ec0c0a720ab80bd7ec4a2eb313e3
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-06-20 19:10:41 +0530

    Update: Reduced top spacing from HERO

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Hero = ({ title, content, img }) => {
   return (
     <>
-      <div className="container col-xxl-8 px-4 py-5 text-black">
+      {/* <div className="container col-xxl-8 px-4 py-5 text-black">
         <div className="row flex-lg-row-reverse align-items-center g-5 ">
-          {/* Image  */}
+          {/* Image  
           <div className="col-10 col-sm-8 col-lg-6">
             <img
               src={img}
@@ -13,6 +13,23 @@ const Hero = ({ title, content, img }) => {
               alt="Image"
               width="700"
               height="500"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold lh-1 mb-3">{title}</h1>
+            <p className="lead text-black">{content}</p>
+          </div>
+        </div>
+      </div> */}
+      <div className="container col-xxl-8 px-4 py-2 text-black ">
+        <div className="row flex-lg-row-reverse align-items-center g-3">
+          {/* Image */}
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img
+              src={img}
+              className="d-block mx-lg-auto img-fluid"
+              alt="Image"
               loading="lazy"
             />
           </div>
